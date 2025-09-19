cc7f5070bca285f9e8b6627acb5dee8867a226dc
MohammadKashifSarwar <40018295+MohammadKashifSarwar@users.noreply.github.com>
2025-09-19 15:04:20 +0500

    commit

--- JazzcashPortal/obj/Debug/net8.0/JazzcashPortal.AssemblyInfo.cs
+++ JazzcashPortal/obj/Debug/net8.0/JazzcashPortal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JazzcashPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a6a031daba400588fe157e6942d9e195a55b771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f5f9075df69c79fcb2d74a87b45ba6ade4beb98")]
 [assembly: System.Reflection.AssemblyProductAttribute("JazzcashPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JazzcashPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
